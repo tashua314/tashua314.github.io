dfb5fc0298da1ffddcac608d0eac413f89edff14
高橋　雄太 <m1205123ing@gmail.com>
2016-06-10 20:23:19 +0900

    tweak

---
+++ page_make.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#
+# Shell script for creating post 
+# based on jekyll's layout format
+
+# get created date
+date=`date +%Y-%m-%d`
+datetime=`date "+%Y-%m-%d %H:%M:%S"`
+
+# get file title from command line
+# e.g. How to use web worker api
+read -p "Post Title: " title
+# e.g. programming javascript blog
+read -p "Categories: " category
+
+# modify $title for jekyll's default theme
+# (small capital with `-`)
+# e.g. How to use web worker api > how-to-use-web-worker-api
+# 1. convert all to small capital
+titleSmallCapital=`tr "[:upper:]" "[:lower:]" <<< $title`
+# echo $titleSmallCapital
+# 2. split with space
+IFS_ORIGINAL="$IFS"
+IFS=" "
+arr=($titleSmallCapital) #set all words into array 
+IFS="$IFS_ORIGINAL"
+# (debug)
+titleFinal=()
+count=0
+for v in "${arr[@]}"; do
+    if [ $count -eq 0 ] #you need space before and after blackets
+        then
+            titleFinal+=$v
+            count+=1
+    else
+            titleFinal+="-"$v
+            count+=1
+    fi
+done
+
+# create file title
+filetitle=$date-$titleFinal.md
+
+# write default layout to the file
+echo '---' >> _posts/$filetitle
+echo 'layout: post' >> _posts/$filetitle
+echo 'title: '$title >> _posts/$filetitle
+echo 'date: '$datetime >> _posts/$filetitle
+echo 'categories: '$category >> _posts/$filetitle
+echo 'type: "posts"' >> _posts/$filetitle
+echo '---' >> _posts/$filetitle
+
+# open in subilme
+vi _posts/$filetitle
